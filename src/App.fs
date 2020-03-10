ee455badd4cfa6dba2958d66d3dc7c02d23430ea
e_ntyo <entyo.contact@gmail.com>
2020-03-11 01:03:57 +0900

    reset

--- src/App.fs
+++ src/App.fs
@@ -1,13 +1,40 @@
 module App
 
 open Fable.React
+open Fable.Core.JsInterop
 
-let handleChange (ev: Browser.Types.Event) = printfn "%s" ev.Value
+type SetState<'t> = 't -> unit
 
-let vdom =
+let useState<'t> (t: 't): 't * SetState<'t> = import "useState" "react"
+
+let useInputValue (initialValue: string) =
+  let (value, setValue) = useState (initialValue)
+
+  let onChange (e: Browser.Types.Event) =
+    let value: string = e.target?value
+    setValue (value)
+
+  let resetValue() = setValue (System.String.Empty)
+
+  value, onChange, resetValue
+
+let inputRIS (_: obj) =
+  let (value, onChange, resetValue) = useInputValue ""
+  let handleChange (ev: Browser.Types.Event) = printfn "%s" ev.Value
+
+  let placeholder = "TY  - CHAP\nAU  - Islam, Gazi\nPY  - 2014/07/29\nSP  - 1781\nEP  - 1783\nSN  - 978-1-4614-5584-4\nT1  - Social Identity Theory\nER  - "
+
   div []
-    [ textarea [ Props.DOMAttr.OnChange handleChange ] []
-      button [] [ str "消去" ] ]
+    [ textarea
+        [ Props.Value value
+          Props.DOMAttr.OnChange onChange
+          Props.Placeholder placeholder 
+          Props.Rows 8
+          Props.Class "ris-input"
+        ] []
+      button [ Props.DOMAttr.OnClick(fun _ -> resetValue()) ] [ str "消去" ] ]
+
+let vdom = div [] [ ofFunction inputRIS () [] ]
 
 let render() = ReactDom.render (vdom, (Browser.Dom.document.getElementById "root"))
 
