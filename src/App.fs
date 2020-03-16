c99bd3ab79a70380582cf7777b5df52aa6947cca
e_ntyo <entyo.contact@gmail.com>
2020-03-17 02:28:32 +0900

    Hooks忘れた

--- src/App.fs
+++ src/App.fs
@@ -7,16 +7,38 @@ open RIS
 
 type SetState<'t> = 't -> unit
 
+// https://github.com/nojaf/fable-react-hooks-sample/blob/master/src/Bindings.fs#L7
 let useState<'t> (t: 't): 't * SetState<'t> = import "useState" "react"
+
+let useEffect (effect : unit -> Fable.Core.U2<unit, unit -> unit>) (dependsOn : obj array) : unit = import "useEffect" "react"
 
 let useInputValue (initialValue: string) =
   let (value, setValue) = useState (initialValue)
 
   let onChange (e: Browser.Types.Event) =
-    let value: string = e.target?value
+    let value: string = e.target?value   
+    setValue (value)
+
+  let resetValue() = setValue (System.String.Empty)
+
+  value, onChange, resetValue
+
+type Props = { sistStr: string }
+
+let sistStrPreviewer (props: Props) =
+  p [] [str props.sistStr]
+
+let Container () =
+  let (userInput, onUserInputChange, resetInputValue) = useInputValue ""
+  let (sistStr, setSistStr) = useState ""
+  let placeholder =
+    "TY  - CHAP\nAU  - Islam, Gazi\nPY  - 2014/07/29\nSP  - 1781\nEP  - 1783\nSN  - 978-1-4614-5584-4\nT1  - Social Identity Theory\nER  - "
+  
+  // https://github.com/nojaf/fable-react-hooks-sample/blob/master/src/UseEffect.fs#L28
+  useEffect (fun () ->
     let headerRecordsPerser = (HeaderParser |> RISHeaderFieldParser) |> Parsimmon.many
     let awesomeParser = (Parsimmon.seq2 headerRecordsPerser RISRecordParser) |> Parsimmon.map snd
-    let result = awesomeParser.parse value
+    let result = awesomeParser.parse userInput
     if result.status then
       let grouped =
         (result.value
@@ -113,7 +135,7 @@ let useInputValue (initialValue: string) =
         + "."
 
       // https://jipsti.jst.go.jp/sist/handbook/sist02sup/sist02sup.htm#5.1.1
-      let result =
+      let sist =
         [ [ authorPart; titlePart ]
           |> List.filter Option.isSome
           |> List.map (fun fa -> fa.Value)
@@ -121,28 +143,22 @@ let useInputValue (initialValue: string) =
         |> List.concat
         |> String.concat ". "
 
-      printfn "%s" result
-    setValue (value)
-
-  let resetValue() = setValue (System.String.Empty)
-
-  value, onChange, resetValue
-
-let inputRIS (_: obj) =
-  let (value, onChange, resetValue) = useInputValue ""
-  let placeholder =
-    "TY  - CHAP\nAU  - Islam, Gazi\nPY  - 2014/07/29\nSP  - 1781\nEP  - 1783\nSN  - 978-1-4614-5584-4\nT1  - Social Identity Theory\nER  - "
+      setSistStr sist |> Fable.Core.U2.Case1
+    else setSistStr "Failed to parse" |> Fable.Core.U2.Case1
+  ) [| userInput |]
 
   div []
     [ textarea
-        [ Props.Value value
-          Props.DOMAttr.OnChange onChange
+        [ Props.Value userInput
+          Props.DOMAttr.OnChange onUserInputChange
           Props.Placeholder placeholder
           Props.Rows 15
           Props.Class "ris-input" ] []
-      button [ Props.DOMAttr.OnClick(fun _ -> resetValue()) ] [ str "消去" ] ]
+      button [ Props.DOMAttr.OnClick(fun _ -> resetInputValue()) ] [ str "消去" ]
+      sistStrPreviewer({ sistStr = sistStr }) ]
 
-let vdom = div [] [ ofFunction inputRIS () [] ]
+
+let vdom = div [] [ ofFunction Container () [] ]
 
 let render() = ReactDom.render (vdom, (Browser.Dom.document.getElementById "root"))
 
